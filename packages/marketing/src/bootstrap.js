eabd4773a4467a030736ae1c85a92fd377a3f91b
panaccius <paterson.anaccius@truepill.com>
2023-01-11 01:02:40 -0300

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+function mount(element) {
+  ReactDOM.render(<App />, element);
+}
+
+if (process.env.NODE_ENV === "development") {
+  const element = document.querySelector("#_marketing-dev-root");
+  if (element) mount(element);
+}
+
+export { mount };
